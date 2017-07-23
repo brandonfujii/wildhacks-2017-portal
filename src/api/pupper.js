fc35fe75f2ddf6dee0990ad183d7bc530d4a7886
brandonfujii <brandonfujii2018@u.northwestern.edu>
2017-07-22 21:04:10 -0400

    added pupper fetch

---
+++ src/api/pupper.js
@@ -0,0 +1,64 @@
+
+const appendQueryParams = (path, queryParams) => {
+    const e = encodeURIComponent;
+    const paramStr = Object.keys(queryParams)
+        .map(key => `${e(key)}=${e(queryParams[key])}`)
+        .join('&');
+
+    const joinedBy = path.indexOf('?') > -1 ? '&' : '?';
+
+    return `${path}${joinedBy}${paramStr}`;
+};
+
+const getUrl = (path, queryParams) => {
+    return queryParams ? appendQueryParams(path, queryParams) : path;
+};
+
+const handleResponse = response => {
+    if (!response.ok) {
+        throw new Error(`HTTP Request failed with error ${response.status}`);
+    }
+
+    return response.json();
+};
+
+class Pupper {
+    constructor(hostname) {
+        this.hostname = hostname ? hostname : process.env.REACT_APP_BACKEND_HOST_NAME;
+        this.defaultHeaders = {
+            'Content-Type': 'application/json',
+        };
+    }
+
+    request(method, route, options = {}) {
+        const path = `${this.hostname}${route}`;
+        const opts = Object.assign(options, {
+            method,
+            headers: {
+                ...this.defaultHeaders,
+                ...options.headers,
+            },
+            body: method !== 'GET' ? JSON.stringify(options.body) : null,
+        });
+
+        return fetch(getUrl(path, opts.queryParams), opts);
+    }
+
+    get(route, options = {}) {
+        return this.request('GET', route, options).then(handleResponse);
+    }
+
+    post(route, options = {}) {
+        return this.request('POST', route, options).then(handleResponse);
+    }
+
+    put(route, options = {}) {
+        return this.request('PUT', route, options).then(handleResponse);
+    }
+
+    delete(route, options = {}) {
+        return this.request('DELETE', route, options).then(handleResponse);
+    }
+}
+
+export default new Pupper();
--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Pupper from './api/pupper';
+
+Pupper
+    .get('/ping', {
+        headers: {
+            'X-Access-Gatekey': 'blah',
+        },
+        body: {
+            someshit: 'idk',
+        }
+    })
+    .then(data => {
+        console.log(data);
+    });
+
+
+const testRegister = () => {
+    Pupper
+        .post('/auth/register', {
+            body: {
+                email: 'test@email.com',
+                password: 'testpass',
+            },
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+};
 
 render(<div>Hello World</div>,
     document.getElementById('root'));
