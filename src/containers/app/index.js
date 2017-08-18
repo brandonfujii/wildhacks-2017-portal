98dcbc5c7403f1147c4a6e937f672196b8262469
Josh Shi <thehandsomepanther@gmail.com>
2017-08-17 23:54:45 -0700

    wordmark svg

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -7,16 +7,9 @@ import Admin from 'containers/admin';
 
 const App = () => (
     <div>
-        <header>
-            <Link to="/">Home</Link>
-            <Link to="/login">Login</Link>
-        </header>
-
-        <main>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Authentication} />
-            <Route exact path="/admin" component={Admin} />
-        </main>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Authentication} />
+        <Route exact path="/admin" component={Admin} />
     </div>
 );
 
--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
+import Wordmark from '../../assets/wordmark';
+
+const Background = styled.div`
+    background-color: #010025;
+    width: 100vw;
+`;
+
+const Section = styled.div`
+    height: 100vh;
+`;
 
 const Home = props => (
-    <div>
-        <h1>Home</h1>
-    </div>
+    <Background>
+        <Section>
+            <Wordmark stroke="#C1AA77" />
+        </Section>
+        <Section />
+    </Background>
 );
 
 const mapStateToProps = state => ({
